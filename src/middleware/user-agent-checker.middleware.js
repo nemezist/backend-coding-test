be3e2fd69fbd7025cd34cd98a20971e573759714
Bagas Prakoso <bagas.ps97@gmail.com>
2021-10-08 21:23:16 +0700

    add npm command for load test

--- src/middleware/user-agent-checker.middleware.js
+++ src/middleware/user-agent-checker.middleware.js
@@ -1,6 +1,6 @@
 const ACCEPTED_USER_AGENTS = [
   'node-superagent/3.8.3', // for supertest
-
+  'Artillery', // for artillery load test
   // other common user-agent
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
   'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
